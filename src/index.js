a616d4fcfd916f54c1b88cd211ca000993a05e63
MiguelEspinozaR <201809447@est.umss.edu>
2026-03-10 16:31:39 -0400

    estructura correcta de arbol de achivos

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// Usamos las rutas
+// Usamos las rutasque son una vrg
 const taskRoutes = require('./routes/task.routes');
 app.use('/tareas', taskRoutes);
 
 app.listen(3000, '0.0.0.0', () => console.log('🚀 Server running on port 3000'));
+
